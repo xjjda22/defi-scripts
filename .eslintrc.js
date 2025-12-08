077baf2b5c913a273757b03ca6d6145d196a780a
harry rana <ranahari@gmail.com>
2025-12-08 17:13:22 +0800

    Add ESLint and Prettier configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  rules: {
+    // Add custom rules here
+  },
+};
